28859da8f97af9f462d232fbd0563cf8b0fc4482
MrxAditya73 <152330610+MrxAditya73@users.noreply.github.com>
2023-12-20 14:04:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['918653805156', 'MR.X ADITYA', true],
-  ['6281255369012', 'Ash', true], 
+  ['917318812338', 'MR.X ADITYA', true], 
   [''] 
 ] //Number of owners
 
